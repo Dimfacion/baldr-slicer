13d587671052ca77705161544c83f20cd45df1f8
Gael Leblan <gael.leblan@yahoo.fr>
2020-09-09 17:35:43 +0200

    Stuff

--- src/components/services/baldrApi.services.js
+++ src/components/services/baldrApi.services.js
@@ -1,59 +1,69 @@
-import Vue from 'vue';
-import VueResource from 'vue-resource';
+import Vue from "vue";
+import VueResource from "vue-resource";
+import pako from "pako";
 
 Vue.use(VueResource);
 
 var slice = function(file, slicerType) {
   let formData = new FormData();
-  formData.append('upload_file', file.item(0))
-  formData.append('id', 123)
+  formData.append("upload_file", file.item(0));
+  formData.append("id", 123);
 
-  return fetch('http://localhost:3000/upload/' + slicerType, {
-      method: 'POST',
-      headers: {
-        'Content-Encoding': 'gzip'
-      },
-      body: formData // This is your file object
-    }).then(response => {
+  var reader = new FileReader();
+  reader.onload = function() {
+    var result = pako.deflate(new Uint8Array(reader.result), { to: "string" });
+
+    console.log(result);
+  };
+  reader.readAsArrayBuffer(file.item(0));
+
+  return fetch("http://localhost:3000/upload/" + slicerType, {
+    method: "POST",
+    headers: {
+      "Content-Encoding": "gzip",
+    },
+    body: formData, // This is your file object
+  })
+    .then((response) => {
       return response;
     })
-    .catch(response => {
+    .catch((response) => {
       console.log(response);
     });
-}
+};
 
 var getPublicProfiles = function() {
-
-  return fetch('http://localhost:3000/public/profiles', {
-      method: 'GET',
-      headers: {
-        'Content-Encoding': 'gzip'
-      }
-    }).then(response => {
+  return fetch("http://localhost:3000/public/profiles", {
+    method: "GET",
+    headers: {
+      "Content-Encoding": "gzip",
+    },
+  })
+    .then((response) => {
       return response;
     })
-    .catch(response => {
+    .catch((response) => {
       console.log(response);
     });
-}
+};
 
 var getConfig = function() {
-
-  return fetch('http://localhost:3000/config', {
-      method: 'GET',
-      headers: {
-        'Content-Encoding': 'gzip'
-      }
-    }).then(response => {
+  return fetch("http://localhost:3000/config", {
+    method: "GET",
+    headers: {
+      "Content-Encoding": "gzip",
+    },
+  })
+    .then((response) => {
       return response;
     })
-    .catch(response => {
+    .catch((response) => {
       console.log(response);
     });
-}
+};
 
 export default {
   slice: slice,
   getPublicProfiles: getPublicProfiles,
-  getConfig: getConfig
-}
+  getConfig: getConfig,
+};
